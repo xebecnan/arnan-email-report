ff236dee53ddf253f561c5d3e5e6e4bc162ecf07
arnan <shenglunan@gmail.com>
2022-06-08 21:12:28 +0800

    改用 gmail 来发邮件

--- send-report.py
+++ send-report.py
@@ -4,11 +4,25 @@ import os
 import sys
 import time
 import smtplib
-import socks
+import base64
+import socket
+import socks as sockswrap
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+from httplib2 import socks
+
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+from google.oauth2.credentials import Credentials
+
 
 MAIL_CFG = 'mail-cfg.txt'
+
+SCOPES = [
+    'https://www.googleapis.com/auth/gmail.readonly',
+    'https://www.googleapis.com/auth/gmail.send',
+]
 
 def loadMailCfg():
     cfg = {}
@@ -23,8 +37,12 @@ cfg = loadMailCfg()
 
 proxy_server = cfg['proxy_server']
 proxy_port = int(cfg['proxy_port'])
+
+sockswrap.setdefaultproxy(sockswrap.PROXY_TYPE_SOCKS5, proxy_server, proxy_port)
+sockswrap.wrapmodule(smtplib)
+
+socket.socket = socks.socksocket
 socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, proxy_server, proxy_port)
-socks.wrapmodule(smtplib)
 
 def genMail():
     mail_title = cfg['mail_title']
@@ -78,9 +96,68 @@ def sendMail(mail_title, mail_body):
         print('sendMail: error',e) #打印错误
 
 
+def getGoogleCreds():
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    return creds
+
+def gmailSendMessage(mail_title, mail_body):
+    """Create and send an email message
+    Print the returned  message id
+    Returns: Message object, including message id
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+
+    mail_host = cfg['smtp']
+    mail_user = cfg['user']
+    mail_pass = cfg['pass']
+    sender    = cfg['sender']
+    mail_to   = cfg['send_to']
+
+    # creds, _ = google.auth.default()
+    creds = getGoogleCreds()
+
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+        message = MIMEText(mail_body)
+        message['To'] = mail_to
+        message['From'] = mail_user
+        message['Subject'] = mail_title
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+        create_message = {
+            'raw': encoded_message
+        }
+        send_message = (service.users().messages().send
+                        (userId="me", body=create_message).execute())
+        print(F'Message Id: {send_message["id"]}')
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        send_message = None
+    return send_message
+
 def main():
     mail_title, mail_body = genMail()
-    sendMail(mail_title, mail_body)
+    # sendMail(mail_title, mail_body)
+    gmailSendMessage(mail_title, mail_body)
 
 if __name__ == '__main__':
     main()
